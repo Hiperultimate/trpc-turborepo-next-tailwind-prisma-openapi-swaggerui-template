722a60925ff206c8b3c76715b5d009cd5b0cb12a
Hiperultimate <hiperultimatevegito@gmail.com>
2023-09-15 14:16:49 +0530

    Fixed build error fetch isse in apps/web

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,15 +1,21 @@
+import { useEffect } from "react";
 import { Button } from "ui";
 import { trpc } from "../utils/trpc";
 
 export default function Home() {
-  // An API call for NextJS Server
-  fetch("http://localhost:3000/api/hello").then((response) =>
-    response.json().then((data) => console.log(data)),
-  );
+  useEffect(() => {
+    const asyncFn = async () => {
+      // An API call for NextJS Server
+      const sendRequest = await fetch("http://localhost:3000/api/hello");
+      const data = await sendRequest.json();
+      console.log(data);
+    };
+
+    asyncFn();
+  }, []);
 
   // API call for apps/api Express tRPC server
-  const { data: exampleCall, isLoading } =
-    trpc.example.exampleInput.useQuery({id : "someStringId"});
+  const { data: exampleCall, isLoading } = trpc.example.exampleInput.useQuery({ id: "someStringId" });
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -17,13 +23,13 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col mx-4">
+      <div className="mx-4 flex flex-col">
         <span className="pt-2 text-lg font-bold ">Web Page</span>
         <div className="my-2">
           <Button />
         </div>
         <div className="flex flex-col">
-          <span className="font-bold my-2">User Details</span>
+          <span className="my-2 font-bold">User Details</span>
           <div>
             ID : <span>{exampleCall?.id}</span>
           </div>
@@ -31,8 +37,7 @@ export default function Home() {
             Name : <span>{exampleCall?.name}</span>
           </div>
           <div>
-            User Session :{" "}
-            <span>{exampleCall?.hasSession ? "true" : "false"}</span>
+            User Session : <span>{exampleCall?.hasSession ? "true" : "false"}</span>
           </div>
         </div>
       </div>
